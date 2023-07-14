0dd33cbb6d0defc52f579b7729af5556b128e187
Tommy Malone <tommymalonegamedev@gmail.com>
2023-07-14 14:23:32 -0700

    Adding basic ProjectileCannon and making all TLMagicProjectile components blueprint readable

--- Source/ActionRoguelike/Public/TLMagicProjectile.h
+++ Source/ActionRoguelike/Public/TLMagicProjectile.h
@@ -20,13 +20,13 @@ public:
 	ATLMagicProjectile();
 
 protected:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USphereComponent* SphereComp;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UProjectileMovementComponent* MovementComp;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UParticleSystemComponent* EffectComp;
 
 
